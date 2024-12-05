9ea31a43d8f1b01a2ef011de034960ec6f843353
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-12-05 18:15:21 +0600

    added delete operation

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete a game review from received _id
+    app.delete("/gameReviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await gameReviewCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
